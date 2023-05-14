de130336d44032f6114fed9753d3033f2b7acbb1
David Rios <david-0214@hotmail.com>
2023-05-13 23:32:47 -0500

    run ok

---
+++ src/main/java/com/co/talataa/TalataaApplication.java
@@ -0,0 +1,13 @@
+package com.co.talataa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalataaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalataaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/talataa/TalataaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.talataa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalataaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
